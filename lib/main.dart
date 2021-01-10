c4b0ac5801eb6f19c2a7c848bacd563f5cbfcef4
Krypton <madhusudanbabar@gmail.com>
2021-01-10 23:02:18 +0530

    scaffold app with stateless widget class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "Simple Interest Calculator",
       home: Scaffold(
         appBar: AppBar(
@@ -11,4 +16,6 @@ void main() => runApp(MaterialApp(
           child: Text("hello world"),
         ),
       ),
-    ));
+    );
+  }
+}
